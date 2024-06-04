35144930a01be2cd180f0d0cbed3afbfdfe88329
poa00 <poa0@icloud.com>
2024-06-04 02:40:00 -0500

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,4 @@
-### PowerShell Profile Refactor
-### Version 1.03 - Refactored
+### Version 0.1
 
 # Initial GitHub.com connectivity check with 1 second timeout
 $canConnectToGitHub = Test-Connection github.com -Count 1 -Quiet -TimeoutSeconds 1
